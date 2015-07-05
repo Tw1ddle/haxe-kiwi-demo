83821826885a1997d4c35ae76dffe683605a4042
sam <sam@silver42.karoo.co.uk>
2015-07-05 19:23:17 +0100

    Minor

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# WORK IN PROGRESS
+
 # Haxe Kiwi Demo
 
 Demo of the Haxe port of the Kiwi constraint solver. Kiwi is an efficient implementation of the algorithm based on the seminal Cassowary paper.
@@ -13,10 +15,6 @@ Open the project in FlashDevelop and build for your preferred target. All target
 ------
 
 Build the app. Press the buttons at the bottom to use it.
-
-![](screenshots/screen1.png?raw=true)
-
-![](screenshots/screen2.png?raw=true)
 
 ### Notes ###
 If the demo does not look like it should or crashes then it may be due to a bug. If problems persist then contact me or open an issue.
--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -5,13 +5,23 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.util.FlxColor;
-import flixel.util.FlxSpriteUtil;
+import haxe.Json;
+import kiwi.Constraint;
+import kiwi.frontend.ConstraintParser;
+import kiwi.frontend.JsonTypes.ConstraintDefinition;
+import kiwi.frontend.Resolver;
+import kiwi.Solver;
 
+using flixel.util.FlxSpriteUtil;
+
+@:access(kiwi.Solver)
 class PlayState extends FlxState {
 	private var uiCamera:FlxCamera;
-	private var problemCount:Int;
-	private var eventText:TextItem;
+	private var problemTick:Int = 0;
+	private var eventText:TextItem = new TextItem(0, 0, "Initializing...", 12);
 	private var graph:FlxSprite;
+	private var resolver:Resolver = new Resolver();
+	private var solver:Solver = new Solver();
 	
 	public function addText(text:String):Void {
 		eventText.text = text + "\n" + eventText.text;
@@ -22,37 +32,69 @@ class PlayState extends FlxState {
 	}
 	
 	public function generateProblem():String {
-		var problem:String = "";
-		
-		problemCount++;
-		
+		var problem:String = '{"constraints":[{"inequality":"x >= 20", "strength":"required"},{"inequality":"y >= 2 + x", "strength":"required"}, {"inequality":"z == y - x", "strength":"required"}]}';
+		problemTick++;
 		return problem;
+	}
+	
+	public function consumeProblem(problem:String):Void {		
+		solver.reset();
+		resolver = new Resolver();
+		
+		var problemDefinition: { constraints:Array<ConstraintDefinition> } = Json.parse(problem);
+		
+		for (constraintDefinition in problemDefinition.constraints) {
+			//trace(constraintDefinition.inequality);
+			var constraint:Constraint = ConstraintParser.parseConstraint(constraintDefinition.inequality, constraintDefinition.strength, resolver);
+			solver.addConstraint(constraint);
+		}
+		
+		solver.updateVariables();
+	}
+	
+	public function graphProblem():Void {
+		graph.drawLine(FlxG.width / 2, 0, FlxG.width / 2, FlxG.height);
+		graph.drawLine(0, FlxG.height / 2, FlxG.width, FlxG.height / 2);
+	}
+	
+	public function outputSolution():Void {
+		var variables = resolver.variables;
+		
+		for (variable in variables) {
+			addText(variable.name + " = " + variable.value);
+		}
 	}
 	
 	public function new():Void {
 		super();
-		destroySubStates = false;
 		
-		problemCount = 0;
-		
-		eventText = new TextItem(0, 0, "Initializing...", 12);
 		add(eventText);
+		
+		//graph = new FlxSprite(0, 0);
+		//graph.screenCenter(true, true);
+		//graph.makeGraphic(FlxG.width, FlxG.height, FlxColor.WHITE);
+		//add(graph);
+		//addText("Created graph graphic...");
 		
 		uiCamera = new FlxCamera(0, 0, Std.int(FlxG.width), Std.int(FlxG.height));
 		uiCamera.bgColor = FlxColor.TRANSPARENT;
 		FlxG.cameras.add(uiCamera);
 		FlxCamera.defaultCameras = [uiCamera];
-		addText("Setup camera...");
-		
-		graph = new FlxSprite();
-		FlxSpriteUtil.screenCenter(graph);
-		add(graph);
-		addText("Created graph graphic...");
+		addText("Setup camera...");		
 		
 		var problem:String = generateProblem();
+		addText("Made problem...");
 		
-		addText("Created problem...");
+		consumeProblem(problem);
+		addText("Consumed problem...");
 		
-		addText("Graphing problem...");
+		addText("Outputting solution...");
+		outputSolution();
+		addText("Done...");
+	}
+	
+	override public function draw():Void {
+		super.draw();
+		//graphProblem();
 	}
 }
